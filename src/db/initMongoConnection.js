2bd824116755ac10486ead9be1779dfe6ab035f3
Zhenya Tishakova <tishakovaevgeniya@gmail.com>
2025-05-25 19:27:39 +0300

    Fix: console.log for db connection

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -10,10 +10,10 @@ export const initMongoConnection = async () => {
     console.log(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`
     );
-    await mongoose.connect(
+    const connection = await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`
     );
-    console.log("Mongo connection successfully established!");
+    console.log("Mongo connection successfully established!", connection);
   } catch (error) {
     console.log("Error while setting up mongo connection", error);
     throw error;
